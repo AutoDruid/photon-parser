944c31b369a4acc07511464c3cb5cfbb08fecec8
Dorian <gaurondorian@gmail.com>
2026-04-07 22:04:40 -0400

    :sparkles: Update names

--- parameters/readers/dicts_test.go
+++ parameters/readers/dicts_test.go
@@ -88,12 +88,12 @@ func TestReadDictionnary(t *testing.T) {
 			got, err := readers.ReadDictionary(reader)
 
 			if (err != nil) != tt.wantErr {
-				t.Errorf("ReadDictionnary() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("ReadDictionary() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 
 			if !tt.wantErr && !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("ReadDictionnary() = %v, want %v", got, tt.want)
+				t.Errorf("ReadDictionary() = %v, want %v", got, tt.want)
 			}
 		})
 	}
@@ -146,12 +146,12 @@ func TestReadHashtable(t *testing.T) {
 			got, err := readers.ReadHashTable(reader)
 
 			if (err != nil) != tt.wantErr {
-				t.Errorf("ReadHashtable() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("ReadHashTable() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 
 			if !tt.wantErr && !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("ReadHashtable() = %v, want %v", got, tt.want)
+				t.Errorf("ReadHashTable() = %v, want %v", got, tt.want)
 			}
 		})
 	}
